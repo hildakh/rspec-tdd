c42696bbc7bb6a2058d8bf04052f9f79d2a3ccb0
hildakh <h_khajehamiri@yahoo.com>
2019-12-15 07:13:43 +0000

    test for allowing passenger added and required method written for class boat

--- boat_spec.rb
+++ boat_spec.rb
@@ -4,4 +4,11 @@ describe Boat do
   it 'should create boats' do
     expect(Boat.new).to be_a Boat
   end
+  describe '#allowed_aboard?' do
+    it 'returns true if inventory includes a life jacket' do
+      a_boat = Boat.new
+      allowed = a_boat.allowed_aboard?(['life jacket', 'sun glasses'])
+      expect(allowed).to be true
+    end
+  end
 end
